60ea2cf804cb6b3bb566549cfd36cfff8a34c420
pukumuna <gs.kabuku@orange.fr>
2025-01-24 12:35:31 +0100

    Pause sur Architecte

--- works.js
+++ works.js
@@ -1,18 +1,18 @@
 //Récupération des pièces eventuellement stockées dans le localStorage
-let works = window.localStorage.getItem('works');
+let worksLocal = window.localStorage.getItem('works');
 
-if (works === null) {
+if (worksLocal === null) {
     // Récupération des works depuis l'API
     const works = await fetch("http://localhost:5678/api/works").then(pieces => pieces.json()); // Fusionner
     //const reponse = await fetch('http://localhost:5678/api/works/');
     //works = await reponse.json();
     
     // Transformation des works en JSON (sérialiser)
-    const valeuWrorks = JSON.stringify(works);
+    const worksBD = JSON.stringify(works);
     // Stockage des informations dans le localStorage (sorte de sauvegarde light)
-    window.localStorage.setItem("works", valeurWorks);
+    window.localStorage.setItem("works", worksBD);
 } else {
-    works = JSON.parse(works);
+    works = JSON.parse(worksLocal);
 }
 console.log("works : " + works);
 for (let i=o; i<works.length; i++) {
